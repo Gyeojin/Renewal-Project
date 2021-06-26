8051eb21b6f651cd970aa097addb17e52c2eda47
Gyeojin <gohyeojin@gmail.com>
2021-06-26 10:29:25 +0900

    210626 / 도트 수정 + 슬라이드 js 진행중

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 
-//const navTop = nav.offsetTop;
-
-//console.log(nav1);
+//Scroll Menu Fixed Code
 
 function fixNav() {
   const nav = document.querySelector('.header');
@@ -14,3 +12,15 @@ function fixNav() {
 }
 
 window.addEventListener('scroll', fixNav);
+
+
+//1.움직여야 하는 것 : 이미지 + 도트 컬러
+const slideImg = document.querySelectorAll('.slide-img');
+const dots = document.querySelectorAll('.dot');
+console.log(slideImg);
+console.log(dots);
+
+
+//2.어떻게? : 자동 가로/세로 슬라이드 + 도트 클릭 시 해당 이미지로 슬라이드
+
+//자동으로 가로로 100%, 세로로 100%씩 움직여야함
