84c152398850e76fc8676f50f4626835161abafc
Gyeojin <gohyeojin@gmail.com>
2021-06-28 18:39:51 +0900

    210628 / 슬라이드 완료 + 중간배너 디자인 완료

--- js/index.js
+++ js/index.js
@@ -15,12 +15,33 @@ window.addEventListener('scroll', fixNav);
 
 
 //1.움직여야 하는 것 : 이미지 + 도트 컬러
-const slideImg = document.querySelectorAll('.slide-img');
-const dots = document.querySelectorAll('.dot');
-console.log(slideImg);
-console.log(dots);
-
+// let current = 0;
+const slidebox = document.querySelector('.slide-img');
+const slideImg = document.querySelectorAll('.slide-img a');
+const dots = document.querySelectorAll('.dot li');
 
 //2.어떻게? : 자동 가로/세로 슬라이드 + 도트 클릭 시 해당 이미지로 슬라이드
-
 //자동으로 가로로 100%, 세로로 100%씩 움직여야함
+
+function slider(values){
+  slidebox.animate (
+    [{left: -100 * values + "%"}] , {duration:400, iterations:1, fill:"forwards"}
+  );
+  // current = values;
+}
+
+// % 나머지 연산자  % 3 0,1,2
+// for(let i = 0; i < dots.length; i++){
+//   console.log(dots[i]);
+// }
+//slider(current);
+
+for(let i = 0; i < dots.length; i++){
+  //console.log(values);
+  dots[i].addEventListener ('click',function(){
+    let values = dots[i].getAttribute("value");
+    // dots.classList.add('dot-click')
+    slider(values);
+  });
+}
+slider(current);
