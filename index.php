9fe09013e0bb342bd7ccc9a2cc9be64c1fe5829b
Gyeojin <gohyeojin@gmail.com>
2021-07-05 18:32:35 +0900

    210705 / php 작업 시작 (헤더 연결)

--- index.php
+++ index.php
@@ -23,73 +23,9 @@
 <body>
   <div class="wrapper">
     <section>
-      <div class="header">
-        <div class="center rel">
-          <a href="#" class="logo"><img src="/renewal/img/logo_baraboda.png" alt="logo"></a>
-          <ul class="nav eng">
-            <li class="menu">
-              <a href="#">바라보다</a>
-              <ul class="submenu">
-                <li>
-                  <a href="#">소개</a>
-                  <a href="#">메뉴 안내</a>
-                </li>
-              </ul>
-            </li>
-            <li class="menu">
-              <a href="#">만나다</a>
-              <ul class="submenu">
-                <li>
-                  <a href="#">오시는 길</a>
-                </li>
-              </ul>
-            </li>
-            <li class="menu">
-              <a href="#">쇼핑하다</a>
-              <ul class="submenu">
-                <li>
-                  <a href="#">스페셜 티</a>
-                  <a href="#">커피팩/선물세트</a>
-                  <a href="#">커피용품</a>
-                  <a href="#">바라보다 굿즈</a>
-                </li>
-              </ul>
-            </li>
-            <li class="menu">
-              <a href="#">마주보다</a>
-              <ul class="submenu">
-                <li>
-                  <a href="#">커피 매거진</a>
-                  <a href="#">질문과 답변</a>
-                  <a href="#">구매 후기</a>
-                </li>
-              </ul>
-            </li>
-            <div class="line"></div>
-            <ul class="notice">
-              <li><a href="#">정기배송</a></li>
-              <li><a href="#">공지사항</a></li>
-              <li><a href="#">이벤트</a></li>
-            </ul>
-          </ul>
-          <div class="icon">
-            <li class="my">
-              <a href="#"><i class="ion-android-person"></i></a>
-              <ul class="submenu">
-                <li>
-                  <a href="#">로그인</a>
-                  <a href="#">회원가입</a>
-                  <a href="#">장바구니</a>
-                  <a href="#">주문 조회</a>
-                  <a href="#">마이 페이지</a>
-                </li>
-              </ul>
-            </li>
-            <a href="#"><i class="ion-bag"></i></a>
-            <a href="#"><i class="icon ion-ios-search"></i></a>
-          </div>
-        </div>
-      </div>
+      <?php
+        include $_SERVER['DOCUMENT_ROOT']."/renewal/include/header.php";
+      ?>
       <div class="banner">
         <div class="slide">
           <div class="slide-img">
